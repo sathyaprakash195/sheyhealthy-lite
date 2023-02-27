53139114491831aafcb872ec8e6641871ceaa2db
sathyaprakash195 <sathyaprakash195@gmail.com>
2023-02-27 19:14:18 +0530

    latest

--- src/pages/DoctorForm/index.js
+++ src/pages/DoctorForm/index.js
@@ -62,6 +62,8 @@ function DoctorForm() {
           form.setFieldsValue(response.data);
           setDays(response.data.days);
         }
+      } else {
+        setAlreadyApplied(false);
       }
       dispatch(ShowLoader(false));
     } catch (error) {
@@ -75,285 +77,282 @@ function DoctorForm() {
   }, []);
   return (
     <div className="bg-white p-2">
-      {!alreadyApplied ||
-        (alreadyApproved && (
-          <>
-            {" "}
-            <h3 className="uppercase my-1">
-              {alreadyApproved
-                ? "Update your information"
-                : "Apply as a doctor"}
-            </h3>
-            <hr />
-            <Form
-              layout="vertical"
-              className="my-1"
-              onFinish={onFinish}
-              form={form}
-            >
-              <Row gutter={[16, 16]}>
-                {/* personal information */}
+      {(!alreadyApplied || alreadyApproved) && (
+        <>
+          {" "}
+          <h3 className="uppercase my-1">
+            {alreadyApproved ? "Update your information" : "Apply as a doctor"}
+          </h3>
+          <hr />
+          <Form
+            layout="vertical"
+            className="my-1"
+            onFinish={onFinish}
+            form={form}
+          >
+            <Row gutter={[16, 16]}>
+              {/* personal information */}
 
-                <Col span={24}>
-                  <h4 className="uppercase">
-                    <b>Personal Information</b>
-                  </h4>
-                </Col>
-                <Col span={8}>
-                  <Form.Item
-                    label="First Name"
-                    name="firstName"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="text" />
-                  </Form.Item>
-                </Col>
-                <Col span={8}>
-                  <Form.Item
-                    label="Last Name"
-                    name="lastName"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="text" />
-                  </Form.Item>
-                </Col>
-                <Col span={8}>
-                  <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="email" />
-                  </Form.Item>
-                </Col>
-                <Col span={8}>
-                  <Form.Item
-                    label="Phone"
-                    name="phone"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="number" />
-                  </Form.Item>
-                </Col>
-                <Col span={8}>
-                  <Form.Item
-                    label="Website"
-                    name="website"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="text" />
-                  </Form.Item>
-                </Col>
-                <Col span={24}>
-                  <Form.Item
-                    label="Address"
-                    name="address"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <textarea type="text" />
-                  </Form.Item>
-                </Col>
+              <Col span={24}>
+                <h4 className="uppercase">
+                  <b>Personal Information</b>
+                </h4>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="First Name"
+                  name="firstName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="text" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Last Name"
+                  name="lastName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="text" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="email" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Phone"
+                  name="phone"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="number" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Website"
+                  name="website"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="text" />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item
+                  label="Address"
+                  name="address"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <textarea type="text" />
+                </Form.Item>
+              </Col>
 
-                <Col span={24}>
-                  <hr />
-                </Col>
+              <Col span={24}>
+                <hr />
+              </Col>
 
-                {/* professional information */}
-                <Col span={24}>
-                  <h4 className="uppercase">
-                    <b>Professional Information</b>
-                  </h4>
-                </Col>
-                <Col span={8}>
-                  <Form.Item
-                    label="Speciality"
-                    name="speciality"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <select>
-                      <option value="dermetologist">Dermetologist</option>
-                      <option value="cardiologist">Cardiologist</option>
-                      <option value="gynecologist">Gynecologist</option>
-                      <option value="neurologist">Neurologist</option>
-                      <option value="orthopedic">Orthopedic</option>
-                      <option value="pediatrician">Pediatrician</option>
-                      <option value="psychiatrist">Psychiatrist</option>
-                      <option value="surgeon">Surgeon</option>
-                      <option value="urologist">Urologist</option>
-                    </select>
-                  </Form.Item>
-                </Col>
+              {/* professional information */}
+              <Col span={24}>
+                <h4 className="uppercase">
+                  <b>Professional Information</b>
+                </h4>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Speciality"
+                  name="speciality"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <select>
+                    <option value="dermetologist">Dermetologist</option>
+                    <option value="cardiologist">Cardiologist</option>
+                    <option value="gynecologist">Gynecologist</option>
+                    <option value="neurologist">Neurologist</option>
+                    <option value="orthopedic">Orthopedic</option>
+                    <option value="pediatrician">Pediatrician</option>
+                    <option value="psychiatrist">Psychiatrist</option>
+                    <option value="surgeon">Surgeon</option>
+                    <option value="urologist">Urologist</option>
+                  </select>
+                </Form.Item>
+              </Col>
 
-                <Col span={8}>
-                  <Form.Item
-                    label="Experience"
-                    name="experience"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="number" />
-                  </Form.Item>
-                </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Experience"
+                  name="experience"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="number" />
+                </Form.Item>
+              </Col>
 
-                <Col span={8}>
-                  <Form.Item
-                    label="Qualification"
-                    name="qualification"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <select>
-                      <option value="MBBS">MBBS</option>
-                      <option value="MD">MD</option>
-                      <option value="MS">MS</option>
-                      <option value="MDS">MDS</option>
-                    </select>
-                  </Form.Item>
-                </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Qualification"
+                  name="qualification"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <select>
+                    <option value="MBBS">MBBS</option>
+                    <option value="MD">MD</option>
+                    <option value="MS">MS</option>
+                    <option value="MDS">MDS</option>
+                  </select>
+                </Form.Item>
+              </Col>
 
-                <Col span={24}>
-                  <hr />
-                </Col>
+              <Col span={24}>
+                <hr />
+              </Col>
 
-                <Col span={24}>
-                  <h4 className="uppercase">
-                    <b>Work Hours</b>
-                  </h4>
-                </Col>
-                {/* work hours */}
-                <Col span={8}>
-                  <Form.Item
-                    label="Start Time"
-                    name="startTime"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="time" />
-                  </Form.Item>
-                </Col>
+              <Col span={24}>
+                <h4 className="uppercase">
+                  <b>Work Hours</b>
+                </h4>
+              </Col>
+              {/* work hours */}
+              <Col span={8}>
+                <Form.Item
+                  label="Start Time"
+                  name="startTime"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="time" />
+                </Form.Item>
+              </Col>
 
-                <Col span={8}>
-                  <Form.Item
-                    label="End Time"
-                    name="endTime"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="time" />
-                  </Form.Item>
-                </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="End Time"
+                  name="endTime"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="time" />
+                </Form.Item>
+              </Col>
 
-                <Col span={8}>
-                  <Form.Item
-                    label="Fee"
-                    name="fee"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Required",
-                      },
-                    ]}
-                  >
-                    <input type="number" />
-                  </Form.Item>
-                </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Fee"
+                  name="fee"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required",
+                    },
+                  ]}
+                >
+                  <input type="number" />
+                </Form.Item>
+              </Col>
 
-                <Col span={24}>
-                  <div className="flex gap-2">
-                    {[
-                      "Monday",
-                      "Tuesday",
-                      "Wednesday",
-                      "Thursday",
-                      "Friday",
-                      "Saturday",
-                      "Sunday",
-                    ].map((day, index) => (
-                      <div className="flex items-center">
-                        <input
-                          type="checkbox"
-                          key={index}
-                          checked={days.includes(day)}
-                          value={day}
-                          onChange={(e) => {
-                            if (e.target.checked) {
-                              setDays([...days, e.target.value]);
-                            } else {
-                              setDays(
-                                days.filter((item) => item !== e.target.value)
-                              );
-                            }
-                          }}
-                        />
-                        <label>{day}</label>
-                      </div>
-                    ))}
-                  </div>
-                </Col>
-              </Row>
+              <Col span={24}>
+                <div className="flex gap-2">
+                  {[
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday",
+                  ].map((day, index) => (
+                    <div className="flex items-center">
+                      <input
+                        type="checkbox"
+                        key={index}
+                        checked={days.includes(day)}
+                        value={day}
+                        onChange={(e) => {
+                          if (e.target.checked) {
+                            setDays([...days, e.target.value]);
+                          } else {
+                            setDays(
+                              days.filter((item) => item !== e.target.value)
+                            );
+                          }
+                        }}
+                      />
+                      <label>{day}</label>
+                    </div>
+                  ))}
+                </div>
+              </Col>
+            </Row>
 
-              <div className="flex justify-end gap-2">
-                <button className="outlined-btn" type="button">
-                  CANCEL
-                </button>
-                <button className="contained-btn" type="submit">
-                  SUBMIT
-                </button>
-              </div>
-            </Form>
-          </>
-        ))}
+            <div className="flex justify-end gap-2">
+              <button className="outlined-btn" type="button">
+                CANCEL
+              </button>
+              <button className="contained-btn" type="submit">
+                SUBMIT
+              </button>
+            </div>
+          </Form>
+        </>
+      )}
 
       {alreadyApplied && !alreadyApproved && (
         <div className="flex flex-col items-center gap-2">
